9ba908c5f320d433e53d7f0a473c44abf8df3fee
queensaldam <queensaldam@gmail.com>
2024-06-27 15:15:09 +0100

    QuadExercise

---
+++ QUAD_EXERCISE.R
@@ -0,0 +1,117 @@
+Equation <- function(a, b, c) {
+  d <- b^2 - 4*a*c
+  
+  x1 <- (-b + sqrt(d)) / (2*a)
+  x2 <- (-b - sqrt(d)) / (2*a)
+  
+    return(paste("The roots are", x1, "and", x2))}
+Q1 <- Equation(1,2, 1)
+Q1
+
+Q2 <- Equation(1,3,-10)
+Q2
+
+Q3 <- Equation(2,5, -3)
+Q3
+
+Q4 <- Equation(1,-4, 4)
+Q4
+
+Q5 <- Equation(1,-6, 9)
+Q5
+
+Q6 <- Equation(3,-5, 2)
+Q6
+
+Q7 <- Equation(1,7, 12)
+Q7
+
+Q8 <- Equation(4,9, 2)
+Q8
+
+Q9 <- Equation(1,-11, 24)
+Q9
+
+Q10 <- Equation(5,-12, 13)
+Q10
+
+Q11 <- Equation(1,8, 15)
+Q11
+
+Q12 <- Equation(2,-10, 12)
+Q12
+
+Q13 <- Equation(3,4, 1)
+Q13
+
+Q14 <- Equation(4,-10,5)
+Q14
+
+Q15 <- Equation(1,-9, 20)
+Q15
+
+Q16 <- Equation(2,-7, 6)
+Q16
+
+Q17 <- Equation(1,-3, 2)
+Q17
+
+Q18 <- Equation(1,-3, 2)
+Q18
+
+Q19 <- Equation(1,1,-4)
+Q19
+
+Q20 <- Equation(2,-7,5)
+Q20
+
+Q21 <- Equation(1,5,-6)
+Q21
+
+Q22 <- Equation(1,4,-9)
+Q22
+
+Q23 <- Equation(3,1,-2)
+Q23
+
+Q24 <- Equation(1,6,-11)
+Q24
+
+Q25 <- Equation(4,2,-3)
+Q25
+
+Q26 <- Equation(1,-8,16)
+Q26
+
+Q27 <- Equation(5,-3,7)
+Q27
+
+Q28 <- Equation(1,9,-18)
+Q28
+
+Q29 <- Equation(2,-6,8)
+Q29
+
+Q30 <- Equation(3,3,-1)
+Q30
+
+Q5 <- Equation(1,-3, 2)
+Q5
+
+Q31 <- Equation(4,-6,7)
+Q31
+
+Q32 <- Equation(1,-7,14)
+Q32
+
+Q33 <- Equation(2,-5,7)
+Q33
+
+Q34 <- Equation(3,-14,15)
+Q34
+
+Q35 <- Equation(1,-9,21)
+Q35
+
+Q36 <- Equation(5,-16,8)
+Q36
